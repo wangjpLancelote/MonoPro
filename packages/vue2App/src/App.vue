f2cf7c072a34014e2f7a920cd13c2a2251ea9f03
Lorenzo <wangjp@szsic.cn>
2024-07-09 11:50:54 +0800

    init monoRepo project

---
+++ packages/vue2App/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>Hello, Vue 2 with Vite and pnpm!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
